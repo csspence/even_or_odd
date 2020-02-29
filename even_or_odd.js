49e6ce8d2a7d5f6bb2687163add966da88355592
Crew Spence <csspence123@gmail.com>
2020-02-29 08:46:41 -0600

    Wrote solution for even_or_odd.

--- even_or_odd.js
+++ even_or_odd.js
@@ -3,6 +3,6 @@ Create a function (or write a script in Shell) that takes an integer as an argum
 "Even" for even numbers or "Odd" for odd numbers.
 */
 
-function even_or_odd(number) {
-  // ...
+const even_or_odd = (number) => {
+  return (number % 2 === 0 ? "Even" : "Odd");
 }
